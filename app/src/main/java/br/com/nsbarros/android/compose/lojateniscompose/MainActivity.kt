0e106ac895b62159ffb214069be6b5d930546805
Nathan Barros <nathansouzabarros@gmail.com>
2025-06-09 01:23:55 -0300

    Criação do módulo de injeção de dependência (Koin).

--- app/src/main/java/br/com/nsbarros/android/compose/lojateniscompose/MainActivity.kt
+++ app/src/main/java/br/com/nsbarros/android/compose/lojateniscompose/MainActivity.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import br.com.nsbarros.android.compose.lojateniscompose.ui.theme.LojaTenisComposeTheme
+import br.com.nsbarros.android.compose.lojateniscompose.ui.theme.home.screen.HomeScreen
+import br.com.nsbarros.android.compose.lojateniscompose.ui.theme.home.viewmodel.HomeViewModel
+import org.koin.androidx.compose.koinViewModel
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +18,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LojaTenisComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val viewModel: HomeViewModel = koinViewModel()
+                val state by viewModel.state.collectAsState()
+
+                HomeScreen(
+                    state = state,
+                    onEvent = viewModel::onEvent
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LojaTenisComposeTheme {
-        Greeting("Android")
     }
 }
