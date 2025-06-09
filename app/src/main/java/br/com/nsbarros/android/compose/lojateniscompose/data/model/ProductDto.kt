0368ed96d428127a1ac583b515281e78c6214c35
Nathan Barros <nathansouzabarros@gmail.com>
2025-06-08 22:29:30 -0300

    Criaçao da classe ProductDto.

---
+++ app/src/main/java/br/com/nsbarros/android/compose/lojateniscompose/data/model/ProductDto.kt
@@ -0,0 +1,9 @@
+package br.com.nsbarros.android.compose.lojateniscompose.data.model
+
+data class ProductDto(
+    val id: String,
+    val name: String,
+    val imageUrl: String,
+    val price: Double,
+    val category: String
+)
