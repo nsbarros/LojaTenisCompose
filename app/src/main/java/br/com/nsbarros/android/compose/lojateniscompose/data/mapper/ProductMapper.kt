46ed744dbcfc16dcf3b56185bf3dde0109e903a1
Nathan Barros <nathansouzabarros@gmail.com>
2025-06-08 22:56:14 -0300

    Adiciona mapper para conversão de ProductDto em Product

---
+++ app/src/main/java/br/com/nsbarros/android/compose/lojateniscompose/data/mapper/ProductMapper.kt
@@ -0,0 +1,17 @@
+package br.com.nsbarros.android.compose.lojateniscompose.data.mapper
+
+import br.com.nsbarros.android.compose.lojateniscompose.data.model.ProductDto
+import br.com.nsbarros.android.compose.lojateniscompose.domain.Product
+
+fun ProductDto.toDomain(): Product {
+    return Product(
+        id = id,
+        name = name,
+        imageUrl = imageUrl,
+        price = price,
+        category = category,
+        description = description,
+        rating = rating,
+        isFavorite = isFavorite
+    )
+}
