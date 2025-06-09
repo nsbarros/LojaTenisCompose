e37e026646772208e35c8e61756d122b32147c17
Nathan Barros <nathansouzabarros@gmail.com>
2025-06-08 22:30:33 -0300

    Revert "Criaçao da classe ProductDto."
    This reverts commit 0368ed96d428127a1ac583b515281e78c6214c35.

--- app/src/main/java/br/com/nsbarros/android/compose/lojateniscompose/data/model/ProductDto.kt
+++
@@ -1,9 +0,0 @@
-package br.com.nsbarros.android.compose.lojateniscompose.data.model
-
-data class ProductDto(
-    val id: String,
-    val name: String,
-    val imageUrl: String,
-    val price: Double,
-    val category: String
-)
