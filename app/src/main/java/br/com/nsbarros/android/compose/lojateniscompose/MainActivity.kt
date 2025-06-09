d5e6e37f9cc0cea6279d87e920212749d09881cf
Nathan Barros <nathansouzabarros@gmail.com>
2025-06-08 22:21:41 -0300

    Inicio do projeto.

---
+++ app/src/main/java/br/com/nsbarros/android/compose/lojateniscompose/MainActivity.kt
@@ -0,0 +1,47 @@
+package br.com.nsbarros.android.compose.lojateniscompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.nsbarros.android.compose.lojateniscompose.ui.theme.LojaTenisComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LojaTenisComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LojaTenisComposeTheme {
+        Greeting("Android")
+    }
+}
